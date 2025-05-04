626d1abab5961123aa5107cfe780a2f82d8c52b9
Mauricio Benjamin <mauriciobenjamin700@gmail.com>
2025-05-04 16:49:25 -0300

    feat: uploading images success

--- src/client/src/app/page.tsx
+++ src/client/src/app/page.tsx
@@ -3,34 +3,65 @@
 import { useState } from "react";
 
 export default function Home() {
-  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [classifyImage, setClassifyImage] = useState<File | null>(null);
+  const [feelingImage, setFeelingImage] = useState<File | null>(null);
   const [isUploading, setIsUploading] = useState(false);
   const [message, setMessage] = useState("");
 
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleSelectClassifyImage = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length > 0) {
-      setSelectedFile(event.target.files[0]);
+      setClassifyImage(event.target.files[0]);
+    }
+  };
+
+  const handleSelectFeelingImage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      setFeelingImage(event.target.files[0]);
     }
   };
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
 
-    if (!selectedFile) {
-      setMessage("Por favor, selecione uma imagem.");
+    if (!feelingImage) {
+      setMessage("Por favor, selecione uma imagem para analisar.");
+      return;
+    }
+    if (!classifyImage) {
+      setMessage("Por favor, selecione uma imagem para classificar.");
       return;
     }
 
     setIsUploading(true);
     setMessage("");
 
-    const formData = new FormData();
-    formData.append("image", selectedFile);
+    const classifyFormData = new FormData();
+    classifyFormData.append("image", feelingImage);
+
+    try {
+      const response = await fetch("/api/classify", {
+        method: "POST",
+        body: classifyFormData,
+      });
+
+      if (response.ok) {
+        setMessage("Imagem enviada com sucesso para a fila!");
+      } else {
+        const errorData = await response.json();
+        setMessage(`Erro: ${errorData.error}`);
+      }
+    } catch (error) {
+      console.error("Erro ao enviar a imagem:", error);
+      setMessage("Erro ao enviar a imagem.");
+    }
+
+    const feelingFormData = new FormData();
+    feelingFormData.append("image", feelingImage);
 
     try {
       const response = await fetch("/api/feeling", {
         method: "POST",
-        body: formData,
+        body: feelingFormData,
       });
 
       if (response.ok) {
@@ -46,16 +77,28 @@ export default function Home() {
       setIsUploading(false);
     }
   };
-
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4">
       <h1 className="text-2xl font-bold mb-4">Enviar Imagem para RabbitMQ</h1>
       <form onSubmit={handleSubmit} className="flex flex-col items-center gap-4">
+        <label htmlFor="classifyImage" className="text-gray-600">
+          Selecione uma imagem para classificar:
+        </label>
         <input
           type="file"
           accept="image/*"
-          onChange={handleFileChange}
+          onChange={handleSelectClassifyImage}
           className="border p-2"
+        />
+        <label htmlFor="feelingImage" className="text-gray-600">
+          Selecione uma imagem para analisar:
+        </label>
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleSelectFeelingImage}
+          className="border p-2"
+          placeholder="Selecione uma imagem para analisar"
         />
         <button
           type="submit"
